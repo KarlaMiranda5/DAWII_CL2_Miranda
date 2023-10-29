c2fed5954ee07ee41738ae42ed7e548645213c05
KarlaMiranda5 <i201720871@cibertec.edu.pe>
2023-10-28 20:26:12 -0500

    2do comit

---
+++ src/test/java/edu/ciberetc/DAWII_CL2_Miranda/DawiiCl2MirandaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ciberetc.DAWII_CL2_Miranda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2MirandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
